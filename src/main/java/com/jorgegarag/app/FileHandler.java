dfb7b01af4e356aca2f0fbdb3f184a82b6acc1f0
Jorge Garcia Aguilar <jorgegarag@gmail.com>
2021-05-28 11:29:03 +0200

    feat(challenge): Changed scanner read from loop to stream to increase readability.

--- src/main/java/com/jorgegarag/app/FileHandler.java
+++ src/main/java/com/jorgegarag/app/FileHandler.java
@@ -5,34 +5,30 @@ import java.util.*;
 
 public class FileHandler {
     private final String splitPattern = "[!\\u0022#$%&()*+,-./:;<=>?@\\u005B\\u005D^_`{|}~\\s\\t\\n\\r]+";
+    private LinkedList<String> sequenceBatch;
+    private Scanner scan;
 
     public void increaseSequenceMapFromFile(File file, int sequence, Map<String, Integer> sequences) throws FileNotFoundException {
-        Scanner scan = new Scanner(file).useDelimiter(splitPattern);
+        scan = new Scanner(file).useDelimiter(splitPattern);
 
-        List<String> sequenceBatch = new ArrayList<>();
+        sequenceBatch = new LinkedList<>();
 
-        for(int i = 0; i < sequence && scan.hasNext(); i++) {
-            String word = scan.next().toLowerCase();
-            sequenceBatch.add(word);
-        }
+        initFirstSequence(sequence, sequences);
 
-        String wordSeqResult = String.join(" ",sequenceBatch);
-        sequences.put(wordSeqResult, sequences.getOrDefault(wordSeqResult, 0) + 1);
+        scan.forEachRemaining( nextWord -> {
+            sequenceBatch.removeFirst();
+            sequenceBatch.add(nextWord.toLowerCase());
 
-        while(scan.hasNext()) {
-            String word = scan.next().toLowerCase();
-            sequenceBatch.remove(0);
-            sequenceBatch.add(word);
-
-            wordSeqResult = String.join(" ",sequenceBatch);
+            String wordSeqResult = String.join(" ", sequenceBatch);
             sequences.put(wordSeqResult, sequences.getOrDefault(wordSeqResult, 0) + 1);
-        }
+            }
+        );
         scan.close();
     }
 
     public File generateTempFileFromInputStream(InputStream source) throws IOException {
         File input = new File("input.txt");
-        FileWriter fw = new FileWriter(input);
+        OutputStreamWriter fw = new FileWriter(input);
         Scanner scanner = new Scanner(source);
         while(scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -42,5 +38,15 @@ public class FileHandler {
         scanner.close();
         fw.close();
         return input;
+    }
+
+    private void initFirstSequence(int sequence, Map<String, Integer> sequences) {
+        for(int i = 0; i < sequence && scan.hasNext(); i++) {
+            String word = scan.next().toLowerCase();
+            sequenceBatch.add(word);
+        }
+
+        String wordSeqResult = String.join(" ",sequenceBatch);
+        sequences.put(wordSeqResult, sequences.getOrDefault(wordSeqResult, 0) + 1);
     }
 }
